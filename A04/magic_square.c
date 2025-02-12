55cb9ed089245b018f0c034801608388070480e6
Hanna A Abrahem <habrahem@goldengate.cs.brynmawr.edu>
2025-02-12 16:22:03 -0500

    added to main magic_square

--- A04/magic_square.c
+++ A04/magic_square.c
@@ -1,10 +1,24 @@
-/*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+/* ----------------------------------------------
+ * Author: Hanna Abrahem 
+ * Date: Febuary 8th, 2025
+ * Description: tests if the given matric is magic square. Where a magic square is NxN, and each row, diagonal has the same sum
  ---------------------------------------------*/
 #include <stdio.h>
 
-int main() {
+/*Adding parameters to read file in command line */
+int main( int argc, char *argv[]) {
+
+	FILE *file = stdin;
+
+	 /* Check if file provided */
+	if (argc == 2) {// command line takes 2 arguments
+        file = fopen(argv[1], "r");
+        if (!file) {
+            perror("Error opening file");
+            return 1;
+        }
+    } 
+
+	
   return 0;
 }
