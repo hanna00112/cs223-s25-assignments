2463369397afc4067ea0a4201f98c1c7acfde656
Hanna A Abrahem <habrahem@goldengate.cs.brynmawr.edu>
2025-02-13 14:16:28 -0500

    started to create linked list for sorted snack

--- A04/sorted_snacks.c
+++ A04/sorted_snacks.c
@@ -1,10 +1,34 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Hanna Abrahem 
+ * Date: Feb 13, 2025
+ * Description: allows users to add snacks to a snackbar, and display them in alabetical order. Use a linked list to store the snacks
  ---------------------------------------------*/
 #include <stdio.h>
+#include <stdlib.h>
+
+/* Node struct */
+struct node {
+	int val;
+	struct node* next;
+};
 
 int main() {
-  return 0;
+	 /* Variable declaration*/
+	 int num_snacks;
+
+	 /* Welcoming message*/
+	 printf("Welcome to Hanna's snackbar. How many snacks would you like to input?:");
+	 scanf("%d", &num_snacks);
+
+	/* Allocating the memory based on how many snacks user inputs */
+	 struct node* n = malloc(sizeof(struct node));
+	 if (n == NULL) {
+		 printf("ERROR: out of space!\n");
+		 exit(1);
+	 }
+
+
+
+
+ 	 return 0;
 }
