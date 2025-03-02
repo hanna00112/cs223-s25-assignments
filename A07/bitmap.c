c4eafb079b17ac624737aa3ea8f7b8d0ee3dcb58
Hanna A Abrahem <habrahem@goldengate.cs.brynmawr.edu>
2025-03-02 01:00:05 -0500

    beginning to work on decoding

--- A07/bitmap.c
+++ A07/bitmap.c
@@ -1,7 +1,7 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Hanna Abrahem 
+ * Date: March 2nd, 2025
+ * Description: reads in a single 64-bit unsigned integer and outputs it as an 8x8 1bbq sprite
  ---------------------------------------------*/
 #include <stdio.h>
 
@@ -11,5 +11,21 @@ int main() {
   printf("Image (unsigned long): %lx\n", img);
 
   // todo: your code here
+  for (int i=0; i < 64; i++) {
+	  unsigned long mask = 0x1ul << (63 - i); //starting in the leftmost bit
+
+	  //checking if its a 0 or 1 
+	  if (img & mask) {
+		  printf("@"); // if 1
+	  } else {
+		  printf(" "); //if 0
+	  }
+
+	  //new line after 8 bits 
+	  if ((i+1) % 8 == 0){
+		  printf("\n");
+	  }
+
+  }
   return 0;
 }
--- A07/decode.c
+++ A07/decode.c
@@ -1,7 +1,7 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Hanna Abrahem 
+ * Date: March 2nd, 2025
+ * Description: reads a PPM file, then outputs any message that mine be stored in the least significant bits of each color
  ---------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,6 +12,21 @@ int main(int argc, char** argv) {
     printf("usage: decode <file.ppm>\n");
     return 0;
   }
+  int width, height;
+  struct ppm_pixel* pixels = read_ppm_2d(argv[1],&width,&height);
+
+  //if could not read pixels 
+  if (!pixels) {
+	  fprintf(stderr, "Error unable to read pixels from file %s", argv[1]);
+	  return 1;
+  }
+  // 3 colors, and 8 bits per character 
+  int max_char = (width * height * 3) / 8;
+  printf("Reading %s with a width: %d and height: %d", argv[1], width, height);
+  printf("Max characters in images: %d", max_char);
+
+  
+
 
   return 0;
 }
