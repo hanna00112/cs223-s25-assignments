840d4c7fe8c8a8fb2a8aadb54165674a060ed64f
Hanna A Abrahem <habrahem@goldengate.cs.brynmawr.edu>
2025-03-06 14:08:44 -0500

    working on decode

--- A07/decode.c
+++ A07/decode.c
@@ -13,20 +13,63 @@ int main(int argc, char** argv) {
     return 0;
   }
   int width, height;
-  struct ppm_pixel* pixels = read_ppm_2d(argv[1],&width,&height);
+  struct ppm_pixel** pixels = read_ppm_2d(argv[1],&width,&height);
+
+
+//  int max_char = (width * height * 3) / 8;
+//  printf("Reading %s with a width: %d and height: %d \n", argv[1], width, height);
+//  printf("Max characters in images: %d \n", max_char);
 
   //if could not read pixels 
   if (!pixels) {
-	  fprintf(stderr, "Error unable to read pixels from file %s", argv[1]);
+	  fprintf(stderr, "Error unable to read pixels from file %s \n", argv[1]);
 	  return 1;
   }
   // 3 colors, and 8 bits per character 
   int max_char = (width * height * 3) / 8;
-  printf("Reading %s with a width: %d and height: %d", argv[1], width, height);
-  printf("Max characters in images: %d", max_char);
+  printf("Reading %s with a width: %d and height: %d \n", argv[1], width, height);
+  printf("Max characters in images: %d \n", max_char);
 
-  
+ // pixel_data = simple 1d array of bites
+ unsigned char* pixel_data = (unsigned char*) pixels; 
 
+
+   printf("First few pixel values:\n");
+   for (int i = 0; i < 10; i++) {  // Print first 10 bytes for debugging
+    printf("%02X ", pixel_data[i]);
+}
+
+ //variables for the loop
+ unsigned char character = 0; //saving new character 
+ int bit_count = 0; //counter for 8 bits 
+
+ //Each byte -- convert to character 
+ for (int i=0; i < width; i++) {
+	 for (int j = 0; j < height; j++) {
+		 unsigned char colors[3] = {pixel.red, pixel.green, pixel.blue};
+
+		 for (int k=0; k < 3; k++) {
+			  //taking least significant bit
+		         character = (character << 1) | (pixel_data[i] & 1); //going through each bit
+        		 //printf("%c", character);
+        		 bit_count++;
+
+			 //once we have 8 bits, convert to character
+	         if (bit_count == 8) {
+                 if (character == '\0') {
+                         break;
+                 }
+                 printf("%c", character);
+                 bit_count = 0; //reseting for next byte
+                 character = 0;
+
+         }
+		 }
+	 }
+ }
+
+ printf("\n");
+ free(pixels);
 
   return 0;
 }
--- A07/read_ppm.c
+++ A07/read_ppm.c
@@ -1,7 +1,7 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Hanna Abrahem  
+ * Date: Feb 16th, 2025
+ * Description: read PPM files stored in binary format. 
  ---------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,6 +14,46 @@ struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
   return NULL;
 }
 
+/*return an array or arrays */
 struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
-  return NULL;
+	FILE* ppmData = fopen(filename, "rb"); //opening file in binary mode
+
+	if (ppmData == NULL) {
+		perror("Error opening file");
+		return NULL;
+	}
+struct ppm_pixel** binary = NULL;
+char buff[1000];
+int index = 0;
+while (fgets(buff, 100, ppmData)) {  
+	if(index == 2) { // 3 line of file is contains width and height
+		sscanf(buff, "%d %d",w,h);
+//	printf(" Width = %d, Height = %d\n", *w, *h);
+	} if(index == 3) { // the rest of the header is unnecessary 	
+		break;
+	}
+	index++;
+}
+
+
+binary = malloc(sizeof(struct ppm_pixel*)* *h);
+
+//alocates memory for each row in 2D array
+for (int i = 0; i < *h; i++) {
+	binary[i] = malloc(sizeof(struct ppm_pixel) * *w);
+	if (binary[i] == NULL) {
+		perror("Error allocating memory");
+		return NULL;
+	}
+	// Read pixel data row by row
+	for (int j=0; j < *w; j++) {
+       		 fread(&binary[i][j], sizeof(struct ppm_pixel), 1, ppmData);
+	}
+}
+
+fclose(ppmData);
+return binary;
+
+return NULL;
+
 }
