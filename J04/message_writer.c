51740b26e7238598e95fc2acdd9e8af8437444a1
Hanna A Abrahem <habrahem@goldengate.cs.brynmawr.edu>
2025-03-26 00:22:44 -0400

    message writer compiles

--- J04/message_writer.c
+++ J04/message_writer.c
@@ -4,94 +4,43 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-/*read meta data with a single call to fread*/
 struct meta_data {
 	float version;
 	unsigned int year;
 	unsigned int length;
 };
-/*
-struct meta_data* reader(const char* filename) {
-	FILE* data = fopen(filename, "rb"); //reading file in binary
-
-	if (data == NULL) {
-		perror("Error opening file");
-		return NULL;
-	}
-	char buffer[256];
-	if (fgets(buffer, sizeof(buffer), data) != NULL){
-		printf("Comment: %s", buffer);
-	} else {
-		printf("Error reading comment\n");
-	}
-
-	struct binary = malloc(sizeof(struct meta_data));
-	if (binary == NULL) {
-		return NULL;
-	}
-	if (freed(binary, sizeof(struct meta_data), 1, data) != 1){
-		free(binary);
-		return NULL;
-	}
-	//allocates memory to message 
-	char* message = malloc(binary->length + 1);
-	if (!message) {
-		free(binary);
-		return NULL;
-	}
-
-	printf("Year: %d /n", meta_data->version);
-
-fclose(data);
-return(binary);
-
-}
-*/
 int main(int argc, char *argv[])
 
 {
-	 FILE* data = fopen(filename, "rb"); //reading file in binary
+	 if (argc != 2) {// command line takes 2 arguments
+                return 1;
+        }
+	 FILE* file = fopen(argv[1], "wb");
+	 if (file == NULL){
+		 perror("Error opening file");
+	 }
+	 //having hardcoded comment
+	 const char* comment = "# This is a test comment";
+	 if (fputs(comment, file)== EOF) {
+		 fprintf(stderr, "Error writing comment");
+		 return 1;
+	 }
+	 //defining meta data
+	 struct meta_data meta = {1.0,2025,15};
+
+	 //Writing meta data
+	 if (fwrite(&meta, sizeof(struct meta_data), 1, file) != 1) {
+		 fprintf(stderr, "Error writing meta data \n");
+		 return 1;
+	 }
+	 const char* message = "Hello World! \n";
+
+	 //writing message data
+	 if (fwrite(message, 1, meta.length, file) != meta.length) {
+		 fprintf(stderr, "Error writing message\n");
+		 return 1;
+	 }
+	 fclose(file);
 	
-	/* Check if file provided */
-        if (argc != 2) {// command line takes 2 arguments
-                return NULL;
-        }
-         if (data == NULL) {
-                perror("Error opening file");
-                return NULL;
-        }
-	//reading comment line
-	char comment[256];
-	if (fgets(comment, 256, data) != 1) {
-		fprintf(stderr, "Error reading comment \n");
-	}
-
-	//read meta data
-	struct meta_data meta;
-	if(fread(&meta, sizeof(meta_data), data) == NULL) {
-		fprintf(stderr, "Error reading data");
-
-	}
-	//allocating memory for message
-	 char* message = malloc(meta->length + 1);
-         if (!message) {
-                return NULL;
-        }
-	//reading message data 
-	if (fread(message, 1, meta.length + 1) != meta.length + 1) {
-		fprintf(stderr, "Error reading message\n");
-      	        free(message);
-                fclose(file);
-	}
-	//printing everything
-	printf("Comment: %s\n", comment);
-	printf("Version: %f\n", meta.version);
-	printf("Year: %d\n", meta.year);
-	printf("Length: %d\n", meta.length);
-	printf("Message: %s\n", message);
-
-	
-
-  return 0;
+return 0;
 }
