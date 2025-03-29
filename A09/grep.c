daa9b9c037650a5be8349b5bd0ca5a83e80f50be
Hanna A Abrahem <habrahem@goldengate.cs.brynmawr.edu>
2025-03-28 20:37:34 -0400

    working on grep

--- A09/grep.c
+++ A09/grep.c
@@ -1,5 +1,83 @@
-#include <stdio.h>
+/***************************************************
+ * Author: Hanna Abrahem 
+ * Date: March 28th, 2025
+ * Description: implements a program that used N processes to search for a keyword in a set of files
+ */
 
-int main() {
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <time.h>       // For timing functions
+#include <sys/time.h>   // For gettimeofday
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+/* argv0 = grep
+ * argv1 = NumProcesses
+ * argv2 = Keyword
+ * arg3 = files  */
+/*N child that searches subset of files, each will open files */
+int main(int argc, char** argv) {
+	if (argv < 4) {
+		printf("Usage: %s <NumProcesses> <Keyword> <Files>\n", argv[0]);
+		return 0;
+	}
+	/*Parsing Command-Line Arguments */
+	int num_processes = atoi(argv[1]); //converting NumProcesses to an integer
+	char* keyword = argv[2];
+	char** files = &argv[3];
+	int num_files = argc - 3; //number of files
+
+	printf("Searching %d files for keyword: %s\n", num_files, keyword);
+
+	/*Subdivide the files among the N processes */
+	int files_per_process = num_files / num_processes;
+	int extra_files = num_files % num_processes; //extra files 
+	int file_index = 0;
+
+	struct timeval start, end;
+	gettimeofday(&start, NULL); //start timing
+
+	pid_t pids[num_processes];
+
+	for (int i = 0; i < num_processes; i++) {
+		int count = files_per_proc + (i < extra_files ? 1 : 0); //distributing extra files
+		pids[i] = fork();
+
+		if (pids[i] == 0) {
+			int matches = 0;
+			printf("Process [%d] searching %d files\n", getpid(), count);
+		}
+		for (int j = 0; j < count; j++) {
+			char* filename = files[files_index++];
+			FILE* file = fopen(filename, "r");
+			if (!file){
+				printf(ANSI_COLOR_RED "Process [%d] Error: Cannot open file %s\n" ANSI_COLOR_RESET, getpid(), filename); //used to make file color red
+			}
+			char line[1024];
+			while (fgets(line, sizeof(line), file)) {
+				if (strstr(line, keyword)) {
+					matches++;
+					printf("Process [%d] Found match in %s: %s", getpid(), filename, line); 
+				}
+			}
+			fclose(file);
+		}
+		exit(matches);
+	}
+
+
+
+
+
   return 0;
 }
