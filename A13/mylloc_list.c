8c5f85684dd0fe1602e17524121f948245512d44
Hanna A Abrahem <habrahem@goldengate.cs.brynmawr.edu>
2025-04-23 22:29:07 -0400

    passing all unit tests

--- A13/mylloc_list.c
+++ A13/mylloc_list.c
@@ -11,10 +11,47 @@ struct chunk *flist = NULL;
 
 void *malloc (size_t size) {
   // TODO: Implement malloc with a free list (See reading for details)
+  if (size == 0) {
+	  return NULL;
+  }
+  struct chunk *ptr = flist;
+  struct chunk *prev = NULL;
+  while (ptr != NULL) {
+	  if (ptr->size >= size) {
+		  if (prev != NULL){
+		  prev->next = ptr->next;
+	  }else {
+		  flist = ptr->next;
+	  }
+	  ptr->used = size;
+	  return (void*)(ptr + 1);
+  } else {
+	  prev = ptr;
+	  ptr = ptr->next;
+  }
+  }
+
+  //requesting memory from system 
+  void *memory = sbrk(size + sizeof(struct chunk));
+  if (memory == (void *)-1){
+	  return NULL;
+  } else {
+	  struct chunk *cnk = (struct chunk*) memory;
+	  cnk->size = size;
+	  cnk->next = NULL;
+	  cnk->used = size;
+	  return (void*)(cnk + 1);
+  }
   return NULL;
 }
 
 void free(void *memory) {
   // TODO: Implement malloc with a free list (See reading for details)
+		if (memory != NULL) {
+			struct chunk *cnk = (struct chunk*)((struct chunk*)memory -1);
+			cnk->next = flist;
+			flist = cnk;
+		}
+	
   return;
 }
